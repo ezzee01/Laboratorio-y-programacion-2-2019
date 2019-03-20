68d166b8232fb66fbdc2f7ed8f5fba60584457dd
ezzee01 <ezec_01@hotmail.com>
2019-03-19 22:08:05 -0300

    Primeros ejercicios clase 2

---
+++ EjerciciosGuia/Ejercicio_04/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_04
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Title = "Ejercicio Nro 04";
+      int suma = 0;
+      int contador = 1;
+
+      for (; ; )
+      {
+        contador++;
+
+        for (int i = 1; i < contador; i++)
+        {
+          if (contador % i == 0)
+          {
+            suma += i;
+          }
+        }
+
+        if (suma == contador)
+        {
+          Console.WriteLine("{0} es numero perfecto. ", contador);
+        }
+
+        suma = 0;
+      }
+    }
+  }
+}
---
+++ EjerciciosGuia/Ejercicio_05/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_05
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Title = "Ejercicio Nro 05";
+
+
+    }
+  }
+}
