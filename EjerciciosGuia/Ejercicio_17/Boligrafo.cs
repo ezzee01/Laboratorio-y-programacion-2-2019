57fd09bd593f5723eb88dbb4adb4ecb8990e8ec8
ezzee01 <ezec_01@hotmail.com>
2019-04-01 22:07:39 -0300

    Ejercicio 16 sin terminar

--- EjerciciosGuia/Ejercicio_17/Boligrafo.cs
+++ EjerciciosGuia/Ejercicio_17/Boligrafo.cs
@@ -11,5 +11,34 @@ namespace Ejercicio_17_2
     private ConsoleColor color = new ConsoleColor();
     private short tinta;
     const short cantidadTintMaxima = 100;
+
+    public Boligrafo(short tinta, ConsoleColor color)
+    {
+      this.color = color;
+      this.tinta = tinta;
+    }
+
+    public short getTinta()
+    {
+      return this.tinta;
+    }
+
+    public ConsoleColor getColor()
+    {
+      return this.color;
+    }
+
+    //public bool Pintar(int gasto, out string dibujo)
+    //{ }
+
+    public void Recargar()
+    {
+      //SetTinta();
+    }
+
+    private void SetTinta(short tinta)
+    {
+      
+    }
   }
 }
