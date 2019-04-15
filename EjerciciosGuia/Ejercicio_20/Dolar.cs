0ac87c792c075652f1d90ea65280494aeb6cca87
ezzee01 <ezec_01@hotmail.com>
2019-04-15 20:29:53 -0300

    ej 31 sin terminar

--- EjerciciosGuia/Ejercicio_20/Dolar.cs
+++ EjerciciosGuia/Ejercicio_20/Dolar.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,113 +6,119 @@ using System.Threading.Tasks;
 
 namespace Billetes
 {
-    class Dolar
+  class Dolar
+  {
+    #region ATRIBUTOS
+    private double cantidad;
+    private static float cotizRespectoDolar;
+    #endregion
+
+    #region CONSTRUCTORES
+    static Dolar()
     {
-        #region ATRIBUTOS
-        private double cantidad;
-        private static float cotizRespectoDolar ;
-        #endregion
 
-        #region CONSTRUCTORES
-        static Dolar()
-        {
-            cotizRespectoDolar = 
-        }
-
-        public Dolar(double cantidad)
-        {
-            this.cantidad = cantidad;
-        }
-
-        public Dolar(double cantidad, float cotizacion) : this(cantidad)
-        { }
-        #endregion
-
-        #region PROPIEDADES
-        public double GetCantidad()
-        {
-            return this.cantidad;
-        }
-
-        public static float GetCotizacion()
-        {
-            return cotizRespectoDolar;
-        }
-        #endregion
-
-        // 1 Euro equivale a 1,13 dólares y 1 dólar equivale a 42,12 pesos  1 e---- 1.13 d
-
-        #region OPERADORES
-        public static explicit operator Euro(Dolar d)
-        {
-        }
-
-        public static explicit operator Pesos(Dolar d)
-        { }
-
-        public static implicit operator Dolar(double d)
-        { }
-
-        public static bool operator !=(Dolar d, Euro e)
-        {
-            return !(d == e);
-        }
-
-        public static bool operator !=(Dolar d, Pesos e)
-        {
-            return !(d == e);
-        }
-
-        public static bool operator !=(Dolar d1, Dolar d2)
-        {
-            return !(d1 == d2);
-        }
-
-        public static Dolar operator -(Dolar d,Euro e)
-        { }
-
-        public static Dolar operator -(Dolar d, Pesos p)
-        { }
-
-        public static Dolar operator +(Dolar d, Euro e)
-        { }
-
-        public static Dolar operator +(Dolar d, Pesos p)
-        { }
-
-        public static bool operator ==(Dolar d, Euro e)
-        {
-            bool retorno = false;
-
-            if(d.cantidad == e.GetCantidad())
-            {
-                retorno = true;
-            }
-
-            return retorno;
-        }
-
-        public static bool operator ==(Dolar d, Pesos p)
-        {
-            bool retorno = false;
-
-            if(d.cantidad == p.GetCantidad())
-            {
-                retorno = true;
-            }
-            return retorno;
-        }
-
-        public static bool operator ==(Dolar d1, Dolar d2)
-        {
-            bool retorno = false;
-
-            if (d1.cantidad == d2.cantidad)
-            {
-                retorno = true;
-            }
-            return retorno;
-        }
-        #endregion
     }
+
+    public Dolar(double cantidad)
+    {
+      this.cantidad = cantidad;
+    }
+
+    public Dolar(double cantidad, float cotizacion) : this(cantidad)
+    { }
+    #endregion
+
+    #region PROPIEDADES
+    public double GetCantidad()
+    {
+      return this.cantidad;
+    }
+
+    public static float GetCotizacion()
+    {
+      return cotizRespectoDolar;
+    }
+    #endregion
+
+    // 1 Euro equivale a 1,13 dólares y 1 dólar equivale a 42,12 pesos  1 e---- 1.13 d
+
+    #region OPERADORES
+    public static explicit operator Euro(Dolar d)
+    {
+      return (Euro)d;
+    }
+
+    public static explicit operator Pesos(Dolar d)
+    {
+      return (Pesos)d;
+    }
+
+    public static implicit operator Dolar(double d)
+    {
+      Dolar dolar = new Dolar(d);
+      return dolar;
+    }
+
+    public static bool operator !=(Dolar d, Euro e)
+    {
+      return !(d == e);
+    }
+
+    public static bool operator !=(Dolar d, Pesos e)
+    {
+      return !(d == e);
+    }
+
+    public static bool operator !=(Dolar d1, Dolar d2)
+    {
+      return !(d1 == d2);
+    }
+
+    public static Dolar operator -(Dolar d, Euro e)
+    { }
+
+    public static Dolar operator -(Dolar d, Pesos p)
+    { }
+
+    public static Dolar operator +(Dolar d, Euro e)
+    { }
+
+    public static Dolar operator +(Dolar d, Pesos p)
+    { }
+
+    public static bool operator ==(Dolar d, Euro e)
+    {
+      bool retorno = false;
+
+      if (d.cantidad == e.GetCantidad())
+      {
+        retorno = true;
+      }
+
+      return retorno;
+    }
+
+    public static bool operator ==(Dolar d, Pesos p)
+    {
+      bool retorno = false;
+
+      if (d.cantidad == p.GetCantidad())
+      {
+        retorno = true;
+      }
+      return retorno;
+    }
+
+    public static bool operator ==(Dolar d1, Dolar d2)
+    {
+      bool retorno = false;
+
+      if (d1.cantidad == d2.cantidad)
+      {
+        retorno = true;
+      }
+      return retorno;
+    }
+    #endregion
+  }
 }
---
+++ EjerciciosGuia/Ejercicio_31/Cliente.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_31
+{
+  class Cliente
+  {
+    #region ATRIBUTOS
+    private string nombre;
+    private int numero;
+    #endregion
+
+    #region CONSTRUCTORES
+    public Cliente(int numero)
+    {
+      this.numero = numero;
+    }
+
+    public Cliente(int numero, string nombre) : this(numero)
+    {
+      this.nombre = nombre;
+    }
+    #endregion
+
+    #region PROPIEDADES
+    public string Nombre
+    {
+      get { return this.nombre; }
+      set { this.nombre = value; }
+    }
+
+    public int Numero
+    {
+      get { return this.numero; }
+    }
+    #endregion
+
+    #region OPERADORES
+    public static bool operator ==(Cliente c1, Cliente c2)
+    {
+      bool retorno = false;
+      if(c1.numero == c2.numero)
+      {
+        retorno = true;
+      }
+      return retorno;
+    }
+
+    public static bool operator !=(Cliente c1, Cliente c2)
+    {
+      return !(c1 == c2);
+    }
+    #endregion
+  }
+}
