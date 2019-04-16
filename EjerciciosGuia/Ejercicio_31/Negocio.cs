2c85d1c2f7d4dc4fbdef435a4dfbf79955d3275f
ezzee01 <ezec_01@hotmail.com>
2019-04-15 22:06:25 -0300

    ej 31 sin terminar

--- EjerciciosGuia/Ejercicio_31/Negocio.cs
+++ EjerciciosGuia/Ejercicio_31/Negocio.cs
@@ -8,5 +8,83 @@ namespace Ejercicio_31
 {
   class Negocio
   {
+    #region ATRIBUTOS
+    private PuestoAtencion caja;
+    private Queue<Cliente> cliente;
+    private string nombre;
+    #endregion
+
+    #region CONTRUCTORES
+    private Negocio()
+    {
+      cliente = new Queue<Cliente>();
+      caja = new PuestoAtencion(PuestoAtencion.Puesto.Caja1);
+    }
+
+    public Negocio(string nombre)
+    {
+      this.nombre = nombre;
+    }
+    #endregion
+
+    #region PROPIEDADES
+    public Cliente Cliente
+    {
+      get { return this.cliente.Dequeue(); }
+      set {  == this.Cliente  }
+    }
+    
+    #endregion
+
+    #region OPERADORES
+    public static bool operator !=(Negocio n, Cliente c)
+    {
+      return !(n == c);
+    }
+
+    public static bool operator ~(Negocio n)
+    {
+      if (n.caja.Atender(n.Cliente))
+      {
+        return true;
+      }
+      else return false;
+    }
+
+    public static bool operator +(Negocio n, Cliente c)
+    {
+      bool retorno = false;
+      foreach(Cliente aux in n.cliente)
+      {
+        if(aux == c)
+        {
+          retorno = false;
+        }
+        else
+        {
+          n.cliente.Enqueue(c);
+          retorno = true;
+        }
+      }
+      return retorno;
+    }
+
+    public static bool operator ==(Negocio n, Cliente c)
+    {
+      bool retorno = false;
+      foreach(Cliente aux in n.cliente)
+      {
+        if(aux == c)
+        {
+          retorno = true;
+        }
+        else
+        {
+          retorno = false;
+        }
+      }
+      return retorno;
+    }
+    #endregion
   }
 }
--- EjerciciosGuia/Ejercicio_31/PuestoAtencion.cs
+++ EjerciciosGuia/Ejercicio_31/PuestoAtencion.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Ejercicio_31
 {
   class PuestoAtencion
   {
+    #region ATRIBUTOS
+    public enum Puesto
+    {
+      Caja1,
+      Caja2
+    }
+    private static int numeroActual;
+    private Puesto puesto;
+    #endregion
+
+    #region PROPIEDADES
+    public static int NumeroActual
+    {
+      get
+      {
+        numeroActual++;
+        return numeroActual;
+      }
+    }
+    #endregion
+
+    #region CONSTRUCTORES Y METODOS
+    static PuestoAtencion()
+    {
+      numeroActual = 0;
+    }
+
+    public PuestoAtencion(Puesto puesto)
+    {
+      this.puesto = puesto;
+    }
+
+    public bool Atender(Cliente cli)
+    {
+      Thread.Sleep(1000);
+      return true;
+    }
+    #endregion
   }
 }
