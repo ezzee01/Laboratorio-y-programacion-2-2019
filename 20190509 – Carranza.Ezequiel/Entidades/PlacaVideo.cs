fe564b9f39eb3228e6602fc99b86b8d32762a895
ezzee01 <ezec_01@hotmail.com>
2019-05-07 21:00:46 -0300

    parcial

---
+++ 20190509 – Carranza.Ezequiel/Entidades/PlacaVideo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class PlacaVideo : Periferico
+    {
+        #region atributos
+        private int ramMB;
+        #endregion
+
+        #region constructores
+        public PlacaVideo(string marca, string modelo, int ram):base(marca,modelo, EConector.PCIExpress)
+        {
+            this.ramMB = ram;
+        }
+        #endregion
+
+        #region metodos
+        public override string ExponerDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendFormat("Memoria: {0}", this.ramMB);
+
+            return sb.ToString();
+        }
+        #endregion
+
+    }
+}
---
+++ 20190509 – Carranza.Ezequiel/Entidades/Teclado.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Teclado : Periferico
+    {
+        #region atributos
+        public enum EDistribucion
+        {
+            Dvorak, QWERTY, QWERTZ, AZERTY
+        }
+
+        private EDistribucion distribucion;
+        #endregion
+
+        #region constructores
+        public Teclado(string marca, string modelo, EConector conector) : this(marca, modelo, conector, EDistribucion.Dvorak)
+        {
+            
+        }
+
+        public Teclado(string marca, string modelo, EConector conector, EDistribucion region) : base(marca,modelo,conector)
+        {
+            this.distribucion = region;
+        }
+        #endregion
+
+        #region operadores
+        public static bool operator !=(Teclado t, EDistribucion distribucion)
+        {
+            return !(t == distribucion);
+        }
+
+        public static bool operator ==(Teclado t, EDistribucion distribucion)
+        {
+            bool retorno = false;
+
+            if(t.distribucion == distribucion)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+        #endregion
+
+        #region metodos
+        public override string ExponerDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Region: {0}", this.distribucion);
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
